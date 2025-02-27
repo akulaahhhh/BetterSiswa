1af5bd351bd266a76133aeca2d0a63ea6fd0a51f
jadMizan <asjadmizan@gmail.com>
2025-02-28 00:21:12 +0800

    create About and Scholarship base content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,13 @@
+import { jacques_francois_shadow, jacques_francois } from "@/app/fonts";
+
 export default function About() {
   return (
     <>
-    <div>
-      From About
-    </div>
+    <main className="w-full min-h-screen flex flex-col max-w-7xl mx-auto px-4 ">
+        <div className="pt-9 flex justify-center">
+          <h2 className={`${jacques_francois.className} text-xl`}>ABOUT</h2>
+        </div>
+      </main>
     </>
   );
 }
--- src/app/scholarship/page.tsx
+++ src/app/scholarship/page.tsx
@@ -1,9 +1,13 @@
+import { jacques_francois_shadow, jacques_francois } from "@/app/fonts";
+
 export default function Scholarship() {
     return (
       <>
-      <div>
-        From Scholarship
-      </div>
+      <main className="w-full min-h-screen flex flex-col max-w-7xl mx-auto px-4 ">
+        <div className="pt-9 flex justify-center">
+          <h2 className={`${jacques_francois.className} text-xl`}>SCHOLARSHIP</h2>
+        </div>
+      </main>
       </>
     );
   }
