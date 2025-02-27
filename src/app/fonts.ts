a02091155c6c43e3041fc16d54d0dd7bd5e15d3c
jadMizan <asjadmizan@gmail.com>
2025-02-27 12:36:02 +0800

    add multiple fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,18 @@
+import { Inter, Jacques_Francois_Shadow , Jacques_Francois} from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const jacques_francois = Jacques_Francois({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+})
+
+export const jacques_francois_shadow = Jacques_Francois_Shadow({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+})
