630a721f83c64156615db2a6803daf2591d8f9dd
jadMizan <asjadmizan@gmail.com>
2025-02-27 12:37:05 +0800

    style main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
 import Image from "next/image";
-import { Navbar } from "@/components/component/navbar";
+import { jacques_francois_shadow, jacques_francois } from "./fonts";
+import Link from "next/link";
 export default function Home() {
   return (
-    <>
-    
-    <div>
-      
-    </div>
-    </>
+    <main className="w-full min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center max-w-7xl mx-auto px-4 -mt-16">
+      <div className="text-center space-y-4">
+        <h1 className={`${jacques_francois_shadow.className} text-8xl md:text-8xl leading-tight`}>
+          BETTER x SISWA
+        </h1>
+        
+        <h2 className={`${jacques_francois.className} text-xl md:text-xl max-w-3xl mx-auto leading-relaxed`}>
+          ENHANCE MALAYSIAN STUDENTS' ACADEMIC PERFORMANCE 
+          <br />
+          AND SHAPING A BETTER FUTURE
+        </h2>
+        <Link href="/about">
+        
+        <button className={`${jacques_francois.className} mt-8 px-8 py-3 bg-black text-white hover:bg-gray-800 transition-colors`}>
+          GET STARTED
+        </button>
+        </Link>
+
+        {/* <div className="mt-16 text-center opacity-50">
+          <p className={`${jacques_francois.className} text-sm`}>
+            ISDFHSILKS ASDD ASDASDSSAD
+          </p>
+          <p className={`${jacques_francois.className} text-xs mt-2`}>
+            ndslknflkSNDlsdsnfknstdkks. AxiasodifinklAAJkdmolkslKlasDlkmmdk
+            <br />
+            ndslknflkSNDlkdsnfknstdkks. AxiasodifinklAAJkdmolkslKlasDlkmmdk
+          </p>
+        </div> */}
+      </div>
+    </main>
   );
 }
