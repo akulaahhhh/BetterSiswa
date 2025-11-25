9959bd59c4a55ba2d6ca470bfce96a4aec688ee3
jadMizan <asjadmizan@gmail.com>
2025-11-25 23:40:57 +0800

    authentication setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,16 @@
-import { clerkMiddleware,createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
+//This matcher will match any URL that starts with /dashboard
+const isStudentDashboardRoute = createRouteMatcher(['/dashboard(.*)'])
+//This matcher wil match any URL that starts with /dashboard/admin * For Admin only
+const isAdminRoute = createRouteMatcher(['/dashboard/admin(.*)'])
 
-// const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
-// const isAdminRoute = createRouteMatcher(['/dashboard/admin(.*)'])
-export default clerkMiddleware();
+export default clerkMiddleware( async(req,auth) =>{
+    //afterauth can be use after clerk has authenticated the user
+    
+    }
+);
 
 export const config = {
   matcher: [
