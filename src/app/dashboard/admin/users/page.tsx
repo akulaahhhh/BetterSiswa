6011145ddd7df94183365686b8c015f70f61d39b
jadMizan <asjadmizan@gmail.com>
2025-11-26 00:26:59 +0800

    deployment error 3rd try

---
+++ src/app/dashboard/admin/users/page.tsx
@@ -0,0 +1,14 @@
+import { jacques_francois } from "@/app/fonts";
+// import { jacques_francois_shadow, jacques_francois } from "@/app/fonts";
+
+export default function Users() {
+  return (
+    <>
+    <main className="w-full min-h-screen flex flex-col max-w-7xl mx-auto px-4 ">
+        <div className="pt-9 flex justify-center">
+          <h2 className={`${jacques_francois.className} text-xl`}>Users</h2>
+        </div>
+      </main>
+    </>
+  );
+}
